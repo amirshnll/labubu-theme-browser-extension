a640338aa79ec4bd1a259de1927aaa6c8adc33b8
Amir Shokri <amirsh.nll@gmail.com>
2025-10-20 11:40:38 +0330

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 find . -type f -name ".DS_Store" -delete
 cd chrome
 zip -r chrome.zip . --exclude ".git/*" --exclude "release.sh" --exclude .DS_Store
-mv chrome.zip ~/Downloads/chrome.zip
+mv chrome.zip ~/Downloads/labubu-chrome.zip
 cd ..
 cd firefox
 zip -r firefox.zip . --exclude ".git/*" --exclude "release.sh" --exclude .DS_Store
-mv firefox.zip ~/Downloads/firefox.zip
+mv firefox.zip ~/Downloads/labubu-firefox.zip
